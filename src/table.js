c47672f3f58b016d5a70312ca738a80c99a39ae7
Khalifa Nassik <knassik@gmail.com>
2012-12-31 16:51:52 +0000

    table.js:  - add function is_DOM() testing if the given node is a dom element  - Table(): add set_node()  - Table_Columns(): add function init()

--- src/table.js
+++ src/table.js
@@ -4,17 +4,17 @@
   var XS;
   
   if ( typeof require === 'function' ) {
-    XS = require( 'xs.js' ).XS;
+    XS = require( './xs.js' ).XS;
   } else {
     XS = exports.XS;
   }
   
-  var log = XS.log
-    , subclass = XS.subclass
-    , extend = XS.extend
-    , Code = XS.Code
+  var log        = XS.log
+    , subclass   = XS.subclass
+    , extend     = XS.extend
+    , Code       = XS.Code
     , Connection = XS.Connection
-    , Set = XS.Set
+    , Set        = XS.Set
   ;
   
   /* -------------------------------------------------------------------------------------------
@@ -27,12 +27,13 @@
   } // ug()
   
   /* -------------------------------------------------------------------------------------------
-     Table_Colunns()
+     Table_Columns()
   */
   function Table_Columns( columns, table, options ) {
     Connection.call( this, options );
     
-    this.table = table;
+    this.table   = table;
+    this.node    = table.node;
     this.columns = columns;
     
     if ( columns instanceof Set ) {
@@ -41,12 +42,34 @@
       this.add( columns );
     }
     
+    this.init();
+    
     return this;
   } // Table_Colunns()
   
   subclass( Connection, Table_Columns );
   
   extend( Table_Columns.prototype, {
+    init: function() {
+      var table   = this.node.getElementsByTagName( "table" )[ 0 ]
+        , columns = this.columns.get()
+        , header  = this.header = table.appendChild( document.createElement( "thead" ) )
+        , l       = columns.length
+      ;
+      
+      for( var i = -1; ++i < l; ) {
+        var c  = columns[ i ]
+          , th = document.createElement( "th" )
+        ;
+        
+        th.appendChild( document.createTextNode( c.label ) );
+        
+        header.appendChild( th );
+      }
+      
+      return this;
+    }, // init()
+    
     add: function( objects ) {
       var table = this.table, a = table.get();
       
@@ -72,7 +95,9 @@
   function Table( node, columns, options ) {
     Set.call( this, options );
     
-    this.columns = new Table_Colunns( columns, this, options );
+    this.set_node( node );
+    
+    this.columns = new Table_Columns( columns, this, options );
     
     return this;
   } // Table()
@@ -80,6 +105,18 @@
   subclass( Set, Table );
   
   extend( Table.prototype, {
+    set_node: function( node ) {
+      if( is_DOM( node ) ) {
+        this.node = node;
+        
+        node.appendChild( document.createElement( "table" ) );
+      } else {
+        throw( "the given node is not a DOM element" );
+      }
+      
+      return this;
+    }, // set_node()
+    
     add: function( objects ) {
       Set.prototype.add.call( this, objects );
       
@@ -103,6 +140,14 @@
      module exports
   */
   eval( XS.export_code( 'XS', [ 'Table' ] ) );
+  
+  // Test if it's a DOM element    
+  function is_DOM( node ){
+    return (
+         typeof HTMLElement === "object" ? node instanceof HTMLElement : node
+      && typeof node === "object" && node.nodeType === 1 && typeof node.nodeName ==="string"
+    );
+  }
   
   de&&ug( "module loaded" );
 } )( this ); // table.js
