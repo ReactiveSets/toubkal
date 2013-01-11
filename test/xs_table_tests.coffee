112d8e12f5831b6cf745f302096454df5b1bb0d0
Khalifa Nassik <knassik@gmail.com>
2013-01-11 11:25:13 +0000

    add fonction from_HTML_to_object() parsing an HTML table to a Javascript object; add test: columns should be equal to object.columns

--- test/xs_table_tests.coffee
+++ test/xs_table_tests.coffee
@@ -2,7 +2,26 @@
 # parse an HTML table to a Javascript object
 # ------------------------------------------
 
-from_HTML_to_object = ( o ) ->
+from_HTML_to_object = ( node ) ->
+  rows    = node.rows
+  columns = []
+  data    = []
+  
+  for cell in rows[ 0 ].cells
+    columns.push { id: cell.getAttribute( "column_id" ), label: cell.innerHTML }
+  
+  for i in [ 1 ... rows.length ]
+    j     = 0
+    o     = {}
+    
+    for cell in rows[ i ].cells
+      v = cell.innerHTML
+      
+      o[ columns[ j++ ].id] = if isNaN( parseInt v ) then v else parseInt v
+    
+    data.push o
+  
+  return { columns: columns, data: data }
 
 # ----------------------------------------------------------------------------------------------
 # xs table unit test suite
@@ -38,41 +57,10 @@ books = new Ordered_Set [
 
 books.table document.getElementById( "demo" ), columns, organizer, {caption: "List of the best-selling books (source: wikipedia)" }
 
-books.add [
-  { id:  5, title: "Angels and Demons"                       , author: "Dan Brown"              , sales:        39, year: 2000, language: "English" }
-  { id:  6, title: "The Girl with the Dragon Tattoo"         , author: "Stieg Larsson"          , sales:        30, year: 2005, language: "Swedish" }
-  { id:  7, title: "The McGuffey Readers"                    , author: "William Holmes McGuffey", sales:       125, year: 1853, language: "English" }
-  { id:  8, title: "The Hobbit"                              , author: "J. R. R. Tolkien"       , sales:       100, year: 1937, language: "English" }
-  { id:  9, title: "The Hunger Games"                        , author: "Suzanne Collins"        , sales:        23, year: 2008, language: "English" }
-  { id: 10, title: "Harry Potter and the Prisoner of Azkaban", author: "J.K. Rowling"           , sales: undefined, year: 1999, language: "English" }
-  { id: 11, title: "The Dukan Diet"                          , author: "Pierre Dukan"           , sales:        10, year: 2000, language: "French"  }
-  { id: 12, title: "Breaking Dawn"                           , author: "Stephenie Meyer"        , sales: undefined, year: 2008, language: "English" }
-  { id: 13, title: "Lolita"                                  , author: "Vladimir Nabokov"       , sales:        50, year: 1955, language: "English" }
-  { id: 14, title: "And Then There Were None"                , author: "Agatha Christie"        , sales:       100, year: undefined, language: "English" }
-  { id: 15, title: "Steps to Christ"                         , author: "Ellen G. White"         , sales:        60, year: null, language: "English" }
-]
-
-columns.add [ { id: "year" , label: "Year", align: "center" }, { id: "language", label: "Language" } ]
-columns.remove [ { id: "id", label: "ID" } ]
-columns.update [
-  [ { id: "language", label: "Language" }, { id: "sales", label: "Sales by millions of copies" } ]
-]
-
-
-books.remove [
-  { id:  1, title: "A Tale of Two Cities", author: "Charles Dickens"        , year: 1859 }
-  { id: 13, title: "Lolita"              , author: "Vladimir Nabokov"       , year: 1955 }
-  { id:  7, title: "The McGuffey Readers", author: "William Holmes McGuffey", year: 1853 }
-]
-
-books.update [
-  [
-    { id:  2, title: "The Lord of the Rings"                            , author: "J. R. R. Tolkien"         , year: 1955 }
-    { id:  2, title: "The Lord of the Rings: The Fellowship of the Ring", author: "John Ronald Reuel Tolkien", year: 1955 }
-  ]
-  [
-    { id: 10, title: "Harry Potter and the Prisoner of Azkaban", author: "J.K. Rowling"  , year: 1999 }
-    { id: 10, title: "Harry Potter and the Prisoner of Azkaban", author: "Joanne Rowling", year: 1999 }
-  ]
-]
-
+describe 'Columns_Set():', ->
+  object = from_HTML_to_object document.getElementsByTagName( "table" )[ 0 ]
+  
+  it 'columns should be equal to object.columns:', ->
+    columns.get().should.be.eql object.columns
+  
+  
--- test/xs_table_tests.js
+++ test/xs_table_tests.js
@@ -2,7 +2,35 @@
 (function() {
   var Ordered_Set, Set, Table, XS, books, chai, columns, from_HTML_to_object, organizer;
 
-  from_HTML_to_object = function(o) {};
+  from_HTML_to_object = function(node) {
+    var cell, columns, data, i, j, o, rows, v, _i, _j, _k, _len, _len1, _ref, _ref1, _ref2;
+    rows = node.rows;
+    columns = [];
+    data = [];
+    _ref = rows[0].cells;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      cell = _ref[_i];
+      columns.push({
+        id: cell.getAttribute("column_id"),
+        label: cell.innerHTML
+      });
+    }
+    for (i = _j = 1, _ref1 = rows.length; 1 <= _ref1 ? _j < _ref1 : _j > _ref1; i = 1 <= _ref1 ? ++_j : --_j) {
+      j = 0;
+      o = {};
+      _ref2 = rows[i].cells;
+      for (_k = 0, _len1 = _ref2.length; _k < _len1; _k++) {
+        cell = _ref2[_k];
+        v = cell.innerHTML;
+        o[columns[j++].id] = isNaN(parseInt(v)) ? v : parseInt(v);
+      }
+      data.push(o);
+    }
+    return {
+      columns: columns,
+      data: data
+    };
+  };
 
   XS = typeof require !== "undefined" && require !== null ? (require('../src/xs.js')).XS : this.XS;
 
@@ -89,162 +117,12 @@
     caption: "List of the best-selling books (source: wikipedia)"
   });
 
-  books.add([
-    {
-      id: 5,
-      title: "Angels and Demons",
-      author: "Dan Brown",
-      sales: 39,
-      year: 2000,
-      language: "English"
-    }, {
-      id: 6,
-      title: "The Girl with the Dragon Tattoo",
-      author: "Stieg Larsson",
-      sales: 30,
-      year: 2005,
-      language: "Swedish"
-    }, {
-      id: 7,
-      title: "The McGuffey Readers",
-      author: "William Holmes McGuffey",
-      sales: 125,
-      year: 1853,
-      language: "English"
-    }, {
-      id: 8,
-      title: "The Hobbit",
-      author: "J. R. R. Tolkien",
-      sales: 100,
-      year: 1937,
-      language: "English"
-    }, {
-      id: 9,
-      title: "The Hunger Games",
-      author: "Suzanne Collins",
-      sales: 23,
-      year: 2008,
-      language: "English"
-    }, {
-      id: 10,
-      title: "Harry Potter and the Prisoner of Azkaban",
-      author: "J.K. Rowling",
-      sales: void 0,
-      year: 1999,
-      language: "English"
-    }, {
-      id: 11,
-      title: "The Dukan Diet",
-      author: "Pierre Dukan",
-      sales: 10,
-      year: 2000,
-      language: "French"
-    }, {
-      id: 12,
-      title: "Breaking Dawn",
-      author: "Stephenie Meyer",
-      sales: void 0,
-      year: 2008,
-      language: "English"
-    }, {
-      id: 13,
-      title: "Lolita",
-      author: "Vladimir Nabokov",
-      sales: 50,
-      year: 1955,
-      language: "English"
-    }, {
-      id: 14,
-      title: "And Then There Were None",
-      author: "Agatha Christie",
-      sales: 100,
-      year: void 0,
-      language: "English"
-    }, {
-      id: 15,
-      title: "Steps to Christ",
-      author: "Ellen G. White",
-      sales: 60,
-      year: null,
-      language: "English"
-    }
-  ]);
-
-  columns.add([
-    {
-      id: "year",
-      label: "Year",
-      align: "center"
-    }, {
-      id: "language",
-      label: "Language"
-    }
-  ]);
-
-  columns.remove([
-    {
-      id: "id",
-      label: "ID"
-    }
-  ]);
-
-  columns.update([
-    [
-      {
-        id: "language",
-        label: "Language"
-      }, {
-        id: "sales",
-        label: "Sales by millions of copies"
-      }
-    ]
-  ]);
-
-  books.remove([
-    {
-      id: 1,
-      title: "A Tale of Two Cities",
-      author: "Charles Dickens",
-      year: 1859
-    }, {
-      id: 13,
-      title: "Lolita",
-      author: "Vladimir Nabokov",
-      year: 1955
-    }, {
-      id: 7,
-      title: "The McGuffey Readers",
-      author: "William Holmes McGuffey",
-      year: 1853
-    }
-  ]);
-
-  books.update([
-    [
-      {
-        id: 2,
-        title: "The Lord of the Rings",
-        author: "J. R. R. Tolkien",
-        year: 1955
-      }, {
-        id: 2,
-        title: "The Lord of the Rings: The Fellowship of the Ring",
-        author: "John Ronald Reuel Tolkien",
-        year: 1955
-      }
-    ], [
-      {
-        id: 10,
-        title: "Harry Potter and the Prisoner of Azkaban",
-        author: "J.K. Rowling",
-        year: 1999
-      }, {
-        id: 10,
-        title: "Harry Potter and the Prisoner of Azkaban",
-        author: "Joanne Rowling",
-        year: 1999
-      }
-    ]
-  ]);
+  describe('Columns_Set():', function() {
+    var object;
+    object = from_HTML_to_object(document.getElementsByTagName("table")[0]);
+    return it('columns should be equal to object.columns:', function() {
+      return columns.get().should.be.eql(object.columns);
+    });
+  });
 
 }).call(this);
