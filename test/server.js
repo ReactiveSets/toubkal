12edbe2aa450f34c0071cbebe8f94db5dfcb5e1f
Jean Hugues Robert <jean_hugues_robert@yahoo.com>
2013-01-25 17:52:51 -0500

    working npm test integerated publisher/subscriber test (read only). persist.js implementation of .file(), written but not debugged.

--- test/server.js
+++ test/server.js
@@ -16,6 +16,8 @@ var server  = Http.createServer( app );
 server.listen( process.env.PORT );
 XS.Proxy.server( server );
 
+var debug_mode = false;
+
 // The fake publisher of the daily mirror has only one subscriber: The Dude.
 var daily_mirror = new xs.set( null, "daily_mirror");
 // daily_mirror.proxy( "daily_mirror_for_the_dude" );
@@ -28,43 +30,88 @@ daily_mirror.add( [
   { id: 1, text: "a first article" },
   { id: 2, text: "another story" }
 ] );
+
+var Articles_Count = 2;
+
 XS.l8.task( function(){
-  var next_id = 3;
+  var next_id = 3;  
   this.repeat( function(){
-    XS.l8.trace( "NEW ARTICLE, " + next_id );
+    if( next_id >  100 ) this.break;
+    XS.l8.trace( "NEW ARTICLE (by publisher), " + next_id );
     daily_mirror.add( [ { id: next_id, text: "article " + next_id } ] );
+    Articles_Count++;
     next_id++;
-    this.sleep( 10 * 1000 );
-  });
-}).label = "daily_mirror acticle generator task"
+    if ( debug_mode ) {
+      this.sleep( 10 * 1000 );
+    } else {
+      this.sleep( 10 );
+    }
+  }).failure( function( e ) { l8.trace( "Error on the publisher: ", e ); } );
+}).label = "daily_mirror acticle generator task";
 
 // daily_mirror.persistor( "daily_mirror.json")
 
-XS.l8.countdown( 1000);
+
 
 var server_url = "http://localhost:" + (process.env.PORT || 8080);
 
 // The Dude subscribe to the daily mirror
 //var daily_mirror_for_the_dude = XS.void.proxy( "daily_mirror_for_the_dude", { url: server_url } );
 
+var total_traces = 0;
 function trace( tracer, model, operation, objects ){
   var buf = [ "[" ];
   for( var item in objects ){
+    total_traces++;
     buf.push( "{" );
     item = objects[ item ];
     for( var property in item ){
-      buf.push( "" + property + ":" + item[ property ] );
+      buf.push( "" + property + ":" + item[ property ] + "," );
     }
-    buf.push( "}");
+    buf.push( "}, ");
   }
-  buf = buf.join( ", " );
-  XS.l8.trace( tracer.toString(), model.toString(), operation, buf );
+  buf.push( "]" );
+  buf = buf.join( "" );
+  XS.l8.trace( "!!!!! " + tracer, model.toString(), operation, buf );
 }
 
 //daily_mirror_for_the_dude.tracer( { log: trace } );
 
 var subscriber = xs.subscribe( "daily_mirror", { url: server_url } );
-subscriber.source.tracer( { log: trace } );
+subscriber.source.tracer( {
+  log:   trace,
+  label: "Tracer for subscriber " + subscriber.source
+} );
+
+!debug_mode && XS.l8.task( function(){
+  var next_id = 1;
+  this.step( function(){ this.sleep( 2000 ); } )
+  this.repeat( function(){
+    if( next_id > 10 ) this.break;
+    // ToDo: JHR, this does not work yet and may never be implemented.
+    subscriber.propose_add( [ { id: next_id, text: "article oops " + next_id } ] );
+    Articles_Count++;
+    XS.l8.trace( "NEW ARTICLE (by subscriber), " + next_id );
+    next_id++;
+    this.sleep( 10 );
+  }).failure( function( e ) { XS.l8.trace( "Error on the subscriber: ", e ); } );
+});
+
+!debug_mode && XS.l8.task( function() {
+  this.step( function(){ this.sleep( 9 * 1000 ) } )
+  this.step( function(){
+    XS.l8.trace( "OK, let's check what the subscriber got: " + total_traces );
+    XS.l8.trace( "I was expecting " + Articles_Count );
+    if( total_traces === Articles_Count ){
+      XS.l8.trace( "SUCCESS!" );
+      process.exit( 0 );
+    }
+    XS.l8.trace( "FAILURE! too bad..." )
+    process.exit( 1 );
+  })
+})
+
+XS.l8.countdown( debug_mode ? 1000 : 10 );
 
 // subscriber.persistor( "daily_mirror_backup" )
 
