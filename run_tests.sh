b0bb4a98272d1830e88d1e6a65efc81b0c330041
Jean Vincent <uiteoi@gmail.com>
2013-02-01 16:41:32 +0000

    run_tests.sh: allow running tests under cygwin without using a symlink until we find a better solution

---
+++ run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Runs tests for cygwin
+#
+# usage: ./run_tests.sh
+#
+# Tests results are then available in test.out
+
+mkdir -p node_modules/excess/lib   && \
+rm    -r node_modules/excess/lib   && \
+mkdir -p node_modules/excess/lib   && \
+cp lib/*js node_modules/excess/lib && \
+npm test > test.out
