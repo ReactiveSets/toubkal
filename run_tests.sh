0e4bc4df6e7df9a7f673c5ba0338c4267821fe02
Jean Vincent <uiteoi@gmail.com>
2013-03-06 00:24:13 +0000

    run_tests.sh: modify script to create link instead of copying files; upgrade npm package

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # Runs tests for cygwin
 #
+# Prior to running the tests, this root directory must contain a node_modules directory
+# Where a link to excess exists. This link is (removed and) created by this script.
+#
 # usage: ./run_tests.sh
 #
 # Tests results are then available in test.out
 
-#mkdir -p node_modules/excess/lib   && \
-#rm    -r node_modules/excess/lib   && \
-#mkdir -p node_modules/excess/lib   && \
-#cp lib/*js node_modules/excess/lib && \
+mkdir -p node_modules
+rm node_modules/excess
+ln -s ../. node_modules/excess
 npm test > test.out
