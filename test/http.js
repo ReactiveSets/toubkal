236a801240616d19c4f3d10c7cd3c4ebf69e97c5
Jean Vincent <uiteoi@gmail.com>
2013-03-08 00:39:13 +0000

    http.js: http server pipelet and first test

---
+++ test/http.js
@@ -0,0 +1,47 @@
+/*  http.js
+    
+    tests for http.js
+    
+    ----
+    
+    Copyright (C) 2013, Connected Sets
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+"use strict";
+
+var XS = require( '../lib/http.js' ).XS
+  , xs         = XS.xs
+  , log        = XS.log
+  , extend     = XS.extend
+;
+
+/* -------------------------------------------------------------------------------------------
+   de&&ug()
+*/
+var de = true;
+  
+function ug( m ) {
+  log( "xs tests http, " + m );
+} // ug()
+  
+/* -------------------------------------------------------------------------------------------
+*/
+xs.set( [
+    { id: 1, ip_address: '0.0.0.0' },
+    { id: 1, port: 8080 }
+  ] )
+  .http_server()
+;
+
