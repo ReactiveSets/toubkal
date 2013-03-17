0e895521e553da6271c5853724ab4e13d53f6e0c
Jean Vincent <uiteoi@gmail.com>
2013-03-17 23:55:02 +0000

    test ec2_availability_zones(), it works.

--- test/ec2.js
+++ test/ec2.js
@@ -1,5 +1,13 @@
 var xs = require( '../lib/server/aws.js' ).XS.xs;
 
-xs.ec2( '../aws-credentials.json' )
+var zones = xs.ec2( '../aws-credentials.json' )
   .ec2_regions()
+  .ec2_availability_zones()
+  .set() // testing output of availability zones
 ;
+
+setTimeout( function() {
+  zones.fetch_all( function( zones ) {
+    console.log( '!!! All zones: ', zones.map( function( z ) { return z.ZoneName } ) )
+  } );
+}, 25000 );
