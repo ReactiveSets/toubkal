99245ce49a12e521c877646f7966a4e868d13919
Jean Vincent <uiteoi@gmail.com>
2013-03-18 10:32:49 +0000

    ec2 tests: use event listener on "complete" to fetch zones content right after receiving the last one

--- test/ec2.js
+++ test/ec2.js
@@ -1,13 +1,17 @@
 var xs = require( '../lib/server/aws.js' ).XS.xs;
 
+require( '../lib/filter.js' );
+
 var zones = xs.ec2( '../aws-credentials.json' )
   .ec2_regions()
+  .filter( function( r ) { return /^us/.test( r.RegionName ) } )
   .ec2_availability_zones()
+  .on( 'complete', function() {
+    zones.fetch_all( function( zones ) {
+      console.log( 'Received all us Availability Zones: '
+        + zones.map( function( z ) { return z.ZoneName } ).join( ', ' )
+      )
+    } );
+  } )
   .set() // testing output of availability zones
 ;
-
-setTimeout( function() {
-  zones.fetch_all( function( zones ) {
-    console.log( '!!! All zones: ', zones.map( function( z ) { return z.ZoneName } ) )
-  } );
-}, 25000 );
