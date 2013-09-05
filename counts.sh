ff577b9cad8ea82df7727c1a1b4019f1d920a891
Jean Vincent <uiteoi@gmail.com>
2013-09-05 14:31:55 +0100

    update source lines counter

--- counts.sh
+++ counts.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-wc \
+wc -l \
   lib/*js \
   lib/server/*js \
-  test/*coffee test/*html test/http.js test/ec2.js \
+  test/*coffee test/*html \
+  test/http.js test/ec2.js test/configuration.js test/images_processing.js \
   *sh package.json README.md todo.txt \
 | sort -n
