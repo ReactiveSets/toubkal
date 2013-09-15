2fc49b14c28313e165c15273a06344aec6d57328
Jean Vincent <uiteoi@gmail.com>
2013-09-15 10:49:08 +0100

    run_tests: no longer make copies, one is now supposed to run "npm link" once

--- run_tests.sh
+++ run_tests.sh
@@ -8,8 +8,8 @@
 #
 # Tests results are then available in test.out
 
-mkdir -p node_modules/excess/lib   && \
-rm    -r node_modules/excess/lib   && \
-mkdir -p node_modules/excess/lib   && \
-cp lib/*js node_modules/excess/lib && \
+#mkdir -p node_modules/excess/lib   && \
+#rm    -r node_modules/excess/lib   && \
+#mkdir -p node_modules/excess/lib   && \
+#cp lib/*js node_modules/excess/lib && \
 npm test > test.out
