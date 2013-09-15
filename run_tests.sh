287e47e7afcbf3abca3e02a94186dafa093cd9fa
Jean Vincent <uiteoi@gmail.com>
2013-09-15 21:17:15 +0100

    remove everyauth dependency, will probably use passport instead

--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 # Runs tests for cygwin
 #
-# Prior to running the tests, this root directory must contain a node_modules directory
-# Where a link to excess exists. This link is (removed and) created by this script.
+# Assumes that node_modules/excess links to this repository root. This can be achieved
+# by running 'npm link' after which 'ls -l node_modules' should show:
+#   excess -> ../.
 #
 # usage: ./run_tests.sh
 #
 # Tests results are then available in test.out
 
-#mkdir -p node_modules/excess/lib   && \
-#rm    -r node_modules/excess/lib   && \
-#mkdir -p node_modules/excess/lib   && \
-#cp lib/*js node_modules/excess/lib && \
 npm test > test.out
