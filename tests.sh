e347b3905bcb59dc255c566359fe5d58dcd09bb3
Jean Vincent <uiteoi@gmail.com>
2013-10-24 12:58:08 +0000

    tests.sh: add node-uuid tests prior to xs_core tests

--- tests.sh
+++ tests.sh
@@ -2,6 +2,7 @@
 coffee -c test/xs_tests_utils.coffee
 coffee -c test/xs_core.coffee
 
+node node_modules/node-uuid/test/test.js && \
 mocha -R spec test/xs_core.js && \
 # add more test suites here
 true
