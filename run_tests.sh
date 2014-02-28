cad8f17bec1567971ad05062228991c4b39a79ff
Jean Vincent <uiteoi@gmail.com>
2014-02-28 17:23:55 +0000

    fix run_tests.sh, to verify that all tests passed using the new mocha test reporter

--- run_tests.sh
+++ run_tests.sh
@@ -9,4 +9,4 @@
 #
 # Tests results are then available in test.out
 
-npm test > test.out && ( tail test.out | grep passing ) || printf "\nfailed\n"
+npm test > test.out && ( tail -n 2 test.out | egrep 'passed ([0-9]*).*of \1[^0-9].*tests' ) || printf "\nfailed\n"
