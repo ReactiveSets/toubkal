3b03d036843c22e60d532c1123a86c94fb847064
Jean Vincent <uiteoi@gmail.com>
2014-03-09 23:54:45 +0000

    search.sh and replace.sh: exclude from find path the directories ./test/javascript and ./test/bootstrap

--- search.sh
+++ search.sh
@@ -5,4 +5,4 @@
 # Usage:
 #  ./search.sh Compose
 
-find './lib' './test' -regextype posix-extended -regex '(.*\.)(coffee|js|html)' -exec grep -nH "$1" {} \;
+find './lib' './test' -not \( -path './test/javascript/*' -o -path './test/bootstrap/*' \) -regextype posix-extended -regex '(.*\.)(coffee|js|html)' -exec grep -nH "$1" {} \;
