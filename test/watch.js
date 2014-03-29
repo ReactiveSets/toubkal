8b07dcaa0283ac6fe9e4cbda8bc671ca5f0cd9db
Jean Vincent <uiteoi@gmail.com>
2014-03-29 21:39:01 +0000

    test/watch.js: more tests for watch_directories()

--- test/watch.js
+++ test/watch.js
@@ -11,33 +11,66 @@ require( '../lib/server/http.js' );
 require( '../lib/server/socket_io_clients.js' );
 require( '../lib/server/file.js' );
 
-var directories = xs
+var entries = xs
   .set( [
 //      { path: 'test' },
-      { path: 'test' },
+//      { path: 'test' },
       { path: 'test' }
     ], { key: [ 'path' ] }
   )
   .watch_directories()
 ;
 
-directories
-  .filter( function( entry ) {
-    return entry.type == 'file' && entry.path.match( /html$/ )
+var files = entries
+  .filter( [ { type: 'file' } ] )
+;
+
+files
+  .filter( [ { type: 'file' } ] )
+  
+  .filter( function( _ ) {
+    return _.path.match( /html$/ );
   } )
   
   .trace( 'html files' )
   .set()
 ;
 
+entries
+  .filter( [ { type: 'directory' } ] )
+  .trace( 'directories' )
+  .set()
+;
+
+files
+  .filter( [ { type: 'file', path: 'test/watch.js' } ] )
+  .trace( 'watch.js' )
+  .set()
+;
+
+files
+  .filter( [ { path: 'test/watch.js' } ] )
+  .trace( 'watch.js 2' )
+  .set()
+;
+
+files
+  .filter( function( _ ) {
+    return _.path.match( /js$/ );
+  } )
+  
+  .trace( 'javascript files' )
+  .set()
+;
+
 setTimeout( remove_directories, 10000 );
 
 function remove_directories() {
-  directories
+  entries
     ._remove( [ 
 //      { path: 'test' },
 //      { path: 'test' },
-      { path: 'test' },
+//      { path: 'test' },
       { path: 'test' }
     ] )
   ;
