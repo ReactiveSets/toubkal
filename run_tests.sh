fea71617edb8064c260b0a4f4a1d7f7ec20b18f1
Jean Vincent <uiteoi@gmail.com>
2014-03-31 08:02:03 -0400

    run_tests.sh: tail more lines to allow for forever stop messages

--- run_tests.sh
+++ run_tests.sh
@@ -9,4 +9,4 @@
 #
 # Tests results are then available in test.out
 
-npm test > test.out && ( tail -n 2 test.out | egrep 'passed ([0-9]*).*of \1[^0-9].*tests' ) || printf "\nfailed\n"
+npm test > test.out && ( tail -n 10 test.out | egrep 'passed ([0-9]*).*of \1[^0-9].*tests' ) || printf "\nfailed\n"
