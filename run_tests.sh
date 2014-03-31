2da368063f1e9e7e4e4a928a92eb7a4506c07157
Jean Vincent <uiteoi@gmail.com>
2014-03-31 17:06:00 +0000

    run_tests.sh: display tests results even when tests fail

--- run_tests.sh
+++ run_tests.sh
@@ -9,4 +9,8 @@
 #
 # Tests results are then available in test.out
 
-npm test > test.out && ( tail -n 10 test.out | egrep 'passed ([0-9]*).*of \1[^0-9].*tests' ) || printf "\nfailed\n"
+npm test > test.out || echo " test failed"
+
+echo "Full test results are in test.out"
+
+tail -n 10 test.out | egrep 'passed ([0-9]*).*of \1[^0-9].*tests'
