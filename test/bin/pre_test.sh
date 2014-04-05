2216fcbf280fe0531720d03fb58138df207f4bc0
Jean Vincent <uiteoi@gmail.com>
2014-04-05 14:09:31 -0400

    pre_test.sh: add 2 seconds delay to allow web server to start serving assets

--- test/bin/pre_test.sh
+++ test/bin/pre_test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-which forever > /dev/null 2>&1 && forever start test/http.js || echo 'forever not found, not starting test/http.js' 1>&2
+which forever > /dev/null 2>&1 && forever start test/http.js && sleep 2 || echo 'forever not found, not starting test/http.js' 1>&2
