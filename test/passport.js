800549f2bd797fecc6536f82d84e8ce78b5eef58
knassik <knassik@gmail.com>
2014-04-28 11:15:20 +0100

    fix passport authentication test to use passport() and passport_strategies() pipelets

--- test/passport.js
+++ test/passport.js
@@ -4,8 +4,8 @@ var xs  = require( '..'  )
   , url = require( 'url' )
 ;
 
-// require( '../lib/server/passport.js' );
-// require( '../lib/join.js' );
+require( '../lib/server/passport.js' );
+require( '../lib/join.js' );
 
 var XS  = xs.XS
   , log = XS.log
@@ -16,104 +16,19 @@ var XS  = xs.XS
 
 function ug( message ) { log( 'passport, ' + message ) }
 
-var logger          = require( 'morgan' )
-  , cookie_parser   = require( 'cookie-parser' )
-  , body_parser     = require( 'body-parser' )
-  , session         = require( 'express-session' )
-  , passport        = require( 'passport' )
-  , TwitterStrategy = require( 'passport-twitter' ).Strategy
+var logger        = require( 'morgan' )
+  , cookie_parser = require( 'cookie-parser' )
+  , body_parser   = require( 'body-parser' )
+  , session       = require( 'express-session' )
 ;
 
 module.exports = function( express, session_options, application, __base ) {
-  passport.serializeUser( function( user, done ) {
-    de&&ug( 'passport.serializeUser(), user: ' + log.s( user ) );
-    
-    done( null, user );
-  } );
-  
-  passport.deserializeUser( function( user, done ) {
-    de&&ug( 'passport.deserializeUser(), user' + log.s( user ) );
-    
-    done( null, user );
-  } );
-  
-  passport.use(
-    new TwitterStrategy( {
-        consumerKey   : 'srpPDY5XOggG8iBjO1qOU2i7A'
-      , consumerSecret: 'yBDPnmATAf1AwBrr2hvRqI683gXotnIS8VttzGFBwdAPmeDHmx'
-      , callbackURL   : 'http://146.185.152.167:7001/passport/twitter/callback'
-    },
-    
-    function( token, tokenSecret, profile, done ) {
-      de&&ug( 'new TwitterStrategy(), verify(): ' + log.s( profile ) );
-      
-      return done( null, profile );
-    } )
-  );
-  
-  application
-    .use( logger()                   ) // request logger middleware
-    .use( cookie_parser()            ) // cookie parser
-    .use( body_parser()              ) // extensible request body parser
-    .use( session( session_options ) ) // session management
-    .use( passport.initialize() )
-    .use( passport.session   () )
-  ;
-  
-  var application_routes = {
-    '/passport/twitter': { method: 'GET'
-      , handler: passport.authenticate( 'twitter' )
-    },
-    
-    '/passport/twitter/callback': { method: 'GET'
-      , handler: ( function() {
-          var fn = passport.authenticate( 'twitter', { successRedirect: __base + '/profile', failureRedirect: __base + '/login' } )
-          
-          return function( request, response, next ) {
-            de&&ug( 'passport.authenticate with options' );
-            console.log( "request._passport: ", request._passport );
-            
-            fn( request, response, next );
-          }
-        } )()
-    }
-  };
-  
-  application
-    .use( function( request, response, next ) { 
-      var _url   = url.parse( request.url ).pathname
-        , method = request.method
-        , route  = application_routes[ _url ]
-      ;
-      
-      console.log( response.redirect );
-      
-      if( ! route ) return next();
-      
-      if( route.method !== method ) return next();
-      
-      de&&ug( 'router middleware, session id: ' + request.sessionID + ', url: ' + _url + ', method: ' + method
-        + ', session: ' + log.s( request.session )
-      );
-      
-      response.redirect = function( url ) {
-        de&&ug( 'redirecting to : ' + url );
-        
-        response.writeHead( 302, { location: url } )
-        response.end()
-      };
-      
-      route.handler.call( this, request, response, next );
-    } )
-  ;
-  
-  /*
   XS.notifications = xs.set();
   
   var input                 = xs.pass_through()
-    , user_profile          = input.flow( 'user_profile'          ).set()
-    , user_provider_profile = input.flow( 'user_provider_profile' ).set()
-    , user_provider_email   = input.flow( 'user_provider_email'   ).set( [], { key: [ 'user_id', 'provider_id' ] } )
+    , user_profile          = input.flow( 'user_profile'          ).set().trace( 'user profile out' )
+    , user_provider_profile = input.flow( 'user_provider_profile' ).set().trace( 'user provider profile out' )
+    , user_provider_email   = input.flow( 'user_provider_email'   ).set( [], { key: [ 'user_id', 'provider_id' ] } ).trace( 'user provider email out' )
     
     , output                = xs.union( [ user_profile, user_provider_profile, user_provider_email ] )
     
@@ -136,67 +51,113 @@ module.exports = function( express, session_options, application, __base ) {
     ._on( 'add', config )
   ;
   
+  // config()
   function config( values ) {
     var passport = values[ 0 ];
     
     if( ! passport ) return;
     
-    
-    user_profile._fetch_all( function( values ) { console.log( values ); } );
-  } // config()
-  */
-  
-  /*
-  */
-  
-  // application is an instance of the connect framework
-  /*
-  /*
-  application
-    .use( connect.logger        ()                  ) // request logger middleware
-    .use( connect.cookieParser  ()                  ) // cookie parser
-    .use( connect.bodyParser    ()                  ) // extensible request body parser
-    .use( connect.methodOverride()                  ) // faux HTTP method support
-    .use( connect.session       ( session_options ) ) // session management
-    // .use( passport.initialize() )
-    // .use( passport.session   () )
-  ;
-  
-  
-  function router( request, response, next ) {
-    de&&ug( 'router middleware' );
-    
-    var url    = request.url
-      , method = request.method
-      , message
-    ;
-    
-    switch( method ) {
-      case 'GET':
-             if( url === '/passport/login'  ) message = '<a href="/auth/twitter">Login with Twitter</a>';
-        else if( url === '/passport/signup' ) message = 'Registration Form';
-        else if( url === '/passport/logout' ) message = 'Logout';
-      break;
+    var application_routes = {
+      '/passport/login': { method: 'GET'
+        , handler: function( request, response, next ) {
+            if( request.isAuthenticated() ) {
+              response.redirect( '/passport/profile' );
+            } else {
+              response.end( '<a href="/passport/twitter">Login with Twitter</a>' );
+            }
+          }
+      },
       
-      case 'POST':
-             if( url === '/passport/login'  ) message = 'Authentify user';
-        else if( url === '/passport/signup' ) message = 'Create new User';
-      break;
-    }
+      '/passport/profile': { method: 'GET'
+        , handler: function( request, response, next ) {
+            
+            if( request.isAuthenticated() ) {
+              var user = request.user
+                , res  = '<p><a href="'  + __base + '/logout">logout</a></p>'
+                    
+                    + '<p><img src="'    + user.photo       + '" /></p>'
+                    + '<p>Name: '        + user.name        + '</p>'
+                    + '<p>User ID: '     + user.id          + '</p>'
+                    + '<p>Provider ID: ' + user.provider_id + '</p>'
+              ;
+              
+              response.end( res );
+            } else {
+              response.redirect( '/passport/login' );
+            }
+          }
+      },
+      
+      '/passport/logout': { method: 'GET'
+        , handler: function( request, response, next ) {
+            request.logout();
+            
+            de&&ug( 'logout(), session: ' + log.s( request.session ) );
+            
+            response.redirect( __base + '/login' );
+          }
+      },
+      
+      '/passport/twitter': { method: 'GET'
+        , handler: passport.authenticate( 'twitter' )
+      },
+      
+      '/passport/twitter/callback': { method: 'GET'
+        , handler: passport.authenticate( 'twitter', { successRedirect: __base + '/profile', failureRedirect: __base + '/login' } )
+      }
+    };
     
-    console.log( 'url: ', url, ', method: ', method, ', message: ', message );
+    passport.serializeUser( function( user, done ) {
+      de&&ug( 'passport.serializeUser(), user : ' + log.s( user ) );
+      
+      done( null, user.id );
+    } );
     
-    response.end( message );
+    passport.deserializeUser( function( user_id, done ) {
+      users._fetch_all( function( user_profiles ) {
+        var user = user_profiles[ 0 ];
+        
+        if( ! user ) return done( new Error( 'user with id: ' + user_id + ' not found' ) );
+        
+        de&&ug( 'passport.deserializeUser(), user: ' + log.s( user ) );
+        
+        return done( null, user )
+      }, [ { flow: 'user_profile', id: user_id } ] );
+    } );
+    
+    application
+      .use( logger()                   ) // request logger middleware
+      .use( cookie_parser()            ) // cookie parser
+      .use( body_parser()              ) // extensible request body parser
+      .use( session( session_options ) ) // session management
+      .use( passport.initialize() )
+      .use( passport.session   () )
+      .use( function( request, response, next ) { 
+        var _url   = url.parse( request.url ).pathname
+          , method = request.method
+          , route  = application_routes[ _url ]
+        ;
+        
+        if( ! route ) return next();
+        
+        if( route.method !== method ) return next();
+        
+        de&&ug( 'router middleware, session id: ' + request.sessionID + ', url: ' + _url + ', method: ' + method
+          + ', session: ' + log.s( request.session )
+        );
+        
+        response.redirect = function( url ) {
+          de&&ug( 'redirecting to : ' + url );
+          
+          response.writeHead( 302, { location: url } );
+          
+          response.end();
+        };
+        
+        route.handler.call( this, request, response, next );
+      } )
+    ;
   }
-  
-  
-  
-  application.use( function( request, response, next ) {
-    de&&ug( 'url: ' + request.url );
-    
-    next();
-  } );
-  */
 } // module.exports
 
 function join_strategies( config_strategie, commun_strategie ) {
