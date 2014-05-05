c4301807fc72ec1ae35d2c4185c22aa4f2f445c0
Jean Vincent <uiteoi@gmail.com>
2014-05-05 19:25:16 +0100

    counts.sh: implement using find, keep old version for reference

--- counts.sh
+++ counts.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
+find *sh *.json *.md *.txt .travis.yml lib test bin \
+  -type d \( \
+       -path test/bootstrap \
+    -o -path test/deprecated \
+    -o -path test/lib \
+    -o -path test/javascript \
+    -o -path test/images \
+    -o -path test/css/images \
+  \) \
+  -prune -o \
+  -regextype posix-egrep \
+  -regex '.*[^~]' \
+  -type f \
+  -exec wc -l {} + \
+| sort -n
+
+exit 0
+
+# Previous method:
 wc -l \
   lib/*js \
   lib/server/*js \
   lib/client/*js \
-  test/*html test/*.js \
+  test/README.txt test/*html test/*.js \
   test/src/*coffee \
   test/bin/*sh \
   test/css/*css \
-  *sh package.json README.md todo.txt \
+  test/fixtures/file/config.json \
+  *sh *.json *.md *.txt .travis.yml bin/uuid_v4 \
 | sort -n
