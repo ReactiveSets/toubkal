950f34d8a422b4d8d1ded83f8ce48fec49c5f47f
Jean Vincent <uiteoi@gmail.com>
2014-06-03 09:02:35 +0100

    search.sh, replace.sh: - Allow search from root directory - Exclude '.git*' './node_modules/*' './bin/*' - Allow extensions 'txt', 'md', and 'yml'

--- replace.sh
+++ replace.sh
@@ -5,4 +5,12 @@
 # Usage:
 #   ./replace.sh s/test_replace/test_replaced/
 
-find './lib' './test' -not \(  -path './test/javascript/*' -o -path './test/bootstrap/*' -o -path './test/lib/*' \) -regextype posix-extended -regex '(.*\.)(coffee|js|html)' -exec sed -i -e "$1" {} \;
+find . -not \( \
+     -path '.git*' \
+  -o -path './node_modules/*' \
+  -o -path './bin/*' \
+  -o -path './test/javascript/*' \
+  -o -path './test/bootstrap/*' \
+  -o -path './test/lib/*' \
+\) -regextype posix-extended -regex '(.*\.)(coffee|js|html|txt|md|yml)' \
+-exec sed -i -e "$1" {} \;
--- search.sh
+++ search.sh
@@ -5,4 +5,12 @@
 # Usage:
 #  ./search.sh Compose
 
-find './lib' './test' -not \( -path './test/javascript/*' -o -path './test/bootstrap/*' -o -path './test/lib/*' \) -regextype posix-extended -regex '(.*\.)(coffee|js|html)' -exec grep -nH "$1" {} \;
+find . -not \( \
+     -path '.git*' \
+  -o -path './node_modules/*' \
+  -o -path './bin/*' \
+  -o -path './test/javascript/*' \
+  -o -path './test/bootstrap/*' \
+  -o -path './test/lib/*' \
+\) -regextype posix-extended -regex '(.*\.)(coffee|js|html|txt|md|yml)' \
+-exec grep -nH "$1" {} \;
