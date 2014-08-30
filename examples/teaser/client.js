58dd49006fc094a1b9921f189e704bea161b4d57
Jean Vincent <uiteoi@gmail.com>
2014-08-30 17:14:31 +0200

    Teaser example: - Add column Quantity to table - Display column date instead of year - Remove year attribute from sales.json, now added dynamically using alter() in server.js - id, year, and quantity fieds are now integer instead of strings - Add some comments - Other formatting and minor modifications

--- examples/teaser/client.js
+++ examples/teaser/client.js
@@ -1,21 +1,28 @@
 "use strict";
 
-var xs = XS.xs
-
-  // Define table displayed columns
-  , columns = [
-      { id: 'year'         , label: 'Year'          	}, // First column
-      { id: 'item'         , label: 'Sales'       	}  // Second column
-    ]
-  , $ = XS.$ // from selector.js
-;
-
 function client() {
+  var xs = XS.xs
+    
+    // Define table displayed columns
+    , columns = [
+        { id: 'date'    , label: 'Date'     }, // First column
+        { id: 'item'    , label: 'Item'     }, // Second column
+        { id: 'quantity', label: 'Quantity' }  // Third column
+      ]
+      
+    , $ = XS.$ // from selector.js
+  ;
+  
   xs.socket_io_server()                      // exchange dataflows with server using socket.io
+    
     .flow( 'sales' )                         // Select sales dataflow
-    .filter( [ { year: '2014' } ] )
-    .order ( [ { id: 'date'   } ] )            // Order by date
-    .trace( 'ordered sales' )
+    
+    .filter( [ { year: 2014 } ] )            // Only pull sales for the year 2014
+    
+    .order ( [ { id: 'date'   } ] )          // Order by date
+    
+    .trace( 'ordered sales' )                // Display pulled sales in console.log
+    
     .table( $( '#sales_table' ), columns )   // Display table
   ;
 }
