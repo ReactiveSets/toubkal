ba7e66690fcfb983357560a76f991c315ff7a0ad
Siham Benabbou <sihambenabbou@gmail.com>
2014-08-30 13:47:42 +0100

    sales.json: - update date format so it can be sortable - add more test records (10 in total) - remove the flow attribute as it is defined in the code - add a sale's identifier (id)
    client.js:
    - remove xs.set for the columns variable initialisation
    - remove the by_date declaration and initialisation as they are useless
    - filter by year as documented
    
    index.html:
    - Modify the page title
    - use a div tag called 'sales_table' instead of a table tag in the body section
    
    server.js:
    - cleanup indentation
    - remove unused code
    - set sales flow directly using the configuration pipelet (as shown in the documenation)

--- examples/teaser/client.js
+++ examples/teaser/client.js
@@ -3,22 +3,19 @@
 var xs = XS.xs
 
   // Define table displayed columns
-  , columns = xs.set( [
+  , columns = [
       { id: 'year'         , label: 'Year'          	}, // First column
       { id: 'item'         , label: 'Sales'       	}  // Second column
-    ] )
-    
-  // Organizer to order incoming sales data
-  , by_date = xs.set( [ { id: 'date' } ] )
-  
+    ]
   , $ = XS.$ // from selector.js
 ;
 
 function client() {
-  xs.socket_io_server()                    // exchange dataflows with server using socket.io
-    .flow( 'sales' )                       // Select sales dataflow
-    .order( by_date )                      // Order by date
+  xs.socket_io_server()                      // exchange dataflows with server using socket.io
+    .flow( 'sales' )                         // Select sales dataflow
+    .filter( [ { year: '2014' } ] )
+    .order ( [ { id: 'date'   } ] )            // Order by date
     .trace( 'ordered sales' )
-    .table( $( '#sales_table' ), columns ) // Display table
+    .table( $( '#sales_table' ), columns )   // Display table
   ;
 }
--- examples/teaser/server.js
+++ examples/teaser/server.js
@@ -13,21 +13,9 @@ require( '../../lib/server/file.js' );
 require( '../../lib/server/uglify.js' );
 require( '../../lib/order.js' );
 
-//var xs = require( 'excess' );
-/* -------------------------------------------------------------------------------------------
-   de&&ug()
-*/
-var de = true;
-
-function ug( m ) {
-  log( "xs tests http, " + m );
-} // ug()
-
 var servers = xs
   .set( [ // Define http servers
     { port: '8080', ip_address: '127.0.0.1' } // this application has only one server
-//    { port:443, ip_address: '0.0.0.0', key: 'key string', cert: 'cert string' }
-    // See also "Setting up free SSL on your Node server" http://n8.io/setting-up-free-ssl-on-your-node-server/
   ] )
   .http_servers() // start http servers
 ;
@@ -57,17 +45,15 @@ var all_min_js = xs.union([xs_dependencies, xs
   ] )])
 
   .auto_increment() // Keeps track of files load order by adding an id attribute starting at 1
-
-  //.require_resolve()            // Resolves node module paths
-
+  
   .union( [ xs.set( [             // Add other javascript assets
     { path: 'client.js', id: 13 } // client code must be loaded after excess
   ] ) ] )
-
+  
   .watch()                      // Retrieves files content with realtime updates
-
+  
   .order( [ { id: 'id' } ] )    // Order files by auto_increment order before minifying
-
+  
   .uglify( 'all-min.js', { warnings: false } )       // Minify in realtime using uglify-js and provide "all-min.map" source map
 ;
 
@@ -79,8 +65,6 @@ var all_min_js = xs.union([xs_dependencies, xs
     { path: 'index.html'}
   ] )
 
-  //.watch_directories()     // Retrrieves files list with realtime updates (watching html and css directories)
-
   .watch()                 // Retrieves file content with realtime updates
 
   .union( [ all_min_js ] ) // Add minified assets
@@ -91,9 +75,7 @@ var all_min_js = xs.union([xs_dependencies, xs
 // Start socket servers on all http servers using socket.io
 var clients = servers.socket_io_clients(); // Provide a dataflow of socket.io client connections
 
-var database = xs.configuration( { filepath: 'sales.json' } ); // * The dataflow store of all database transactions
-
-var sales = database.set_flow( 'sales' ); // Dataflow of sales
+var sales = xs.configuration( { filepath: 'sales.json'    , flow: 'sales' } ); // * The dataflow store of all database transactions
 
 sales
   .dispatch( clients, client )  // Serve 64k simultaneous user connexions over one core
