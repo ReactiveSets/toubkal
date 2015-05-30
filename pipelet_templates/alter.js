f9a3bdfaad00448c834c40101368becd934759e6
Jean Vincent <uiteoi@gmail.com>
2015-05-30 18:04:44 +0100

    more comments for pipelet_templates/alter.js

--- pipelet_templates/alter.js
+++ pipelet_templates/alter.js
@@ -22,7 +22,7 @@
 ( this.undefine || require( 'undefine' )( module, require ) )
 
 /*
-   Provide browser options Object here if desired e.g. { global: true, no_conflict: true }
+   Provide browser options Object here if desired e.g. ( { global: true, no_conflict: true } )
    
    For more information and other options, check https://www.npmjs.com/package/undefine
 */
@@ -46,28 +46,54 @@
   
   /* -------------------------------------------------------------------------------------------
      de&&ug()
+     
+     Set de to false to remove all log code, minifiers will typically elliminate dead code
   */
   var de = true, ug = RS.log.bind( null, 'square' );
   
   /* -------------------------------------------------------------------------------------------
      square()
      
-     Computes the square of 'root' attribute of values into 'square' attribute
+     Computes the square of 'root' attribute of values from upstream dataflow into 'square'
+     attribute emitted to downstream dataflow
+  */
+  
+  /*
+     Compose( pipelet_name, function ) creates a new pipelet, which name is square()
+     using function( source, options ) where source is a source dataflow and options
+     are options for the square pipelet.
+     
+     Check Compose() documentation for options.
   */
   Compose( 'square', function( source, options ) {
+  
+    // alter emits a modfied dataflow from the source dataflow
     return source.alter( function( value ) {
+    
+      // alter() provides shallow-cloned values from the source dataflow, preventing most upstream side-effects
+      // modify this clone value in-place to provide the altered value
+      
+      // Do not try / catch unless there is a real possibility of exceptions
       try {
-        value.square = square( value.root );
-      } catch( e ) {
-        log( 'Error while computing square of:', value.root );
+      
+        // The square() function for this template is imported from a dependency it is purely functional
         
+        value.square = square( value.root );
+        
+      } catch( e ) {
+        de&&ug( 'Error while computing square of:', value.root );
+        
+        // Provide error "in-band", downstream
         value.error = e;
       }
-    } );
+      
+      // Do not return anything, alter(), in this form, ignores returned values
+      
+    } /* Alter options here, check alter() documentation for details */ );
   } );
   
   de&&ug( "module loaded" );
   
-  // This module exports a modified rs
+  // This module re-exports rs, modified with the composed square() pipelet
   return rs;
 } ); // square.js
