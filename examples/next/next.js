68765219d1618089fd54fe0cc2d06199b51fb32d
Jean Vincent <uiteoi@gmail.com>
2015-12-17 11:41:24 -0500

    examples/next/next.js: use Multiton to define crankshaft(), updating counters

--- examples/next/next.js
+++ examples/next/next.js
@@ -2,6 +2,7 @@
 ( 'next_test', [ [ 'rs', 'toubkal' ] ], function( rs ) {
   var tag = 'next';
   
+  // Set and display counter set
   rs
     .Singleton( 'counters', function( source, options ) {
       options.key = [ 'id' ];
@@ -10,52 +11,62 @@
         .flow( 'counters', options )
         .set( [ { id: '1', last: 25, description: 'starts at 25' } ] )
         .set_flow( 'counters' )
+        .trace( 'counters' )
       ;
     } )
     
     .counters()
     .order( [ { id: 'id' } ] )
     .table( rs.RS.$( '#next' ), [ { id: 'id' }, { id: 'description' }, { id: 'last' } ] )
-    
+  ;
+  
+  // Define trigger every second
+  rs
     .Singleton( 'trigger', function( source, options ) {
       return source
         .set_flow( 'trigger' )
       ;
     } )
+    
+    .beat( 1000 )
+    //.once( 1000 )
+    .trigger()
   ;
   
-  var out = rs
+  // Define crankshaft to update counters
+  rs  
+    .Multiton( 'crankshaft', function( tag ) { return tag }, function( source, tag, options ) {
+      return source
+        .next( rs.trigger(), function( counter, trigger ) {
+          trigger.id = ++counter.last;
+        }, { fork_tag: tag } )
+        .trace( 'crankshaft' )
+      ;
+    } )
+    
     .counters()
-    .trace( 'counters' )
-    .next( rs.trigger(), function( image, trigger ) {
-      trigger.id = ++image.last;
-    }, { fork_tag: tag } )
-    .trace( 'next' )
-  ;
-  
-  out
+    .crankshaft( tag )
     .flow( 'counters' )
     .trace( 'counters from next' )
     .counters()
   ;
   
-  out
+  // Trace triggers updated by crankshaft
+  rs
+    .crankshaft( tag )
     .flow( 'trigger' )
     .trace( 'trigger' )
     .greedy()
   ;
   
-  rs.union( [
-      out.flow( 'counters' ),
-      out.flow( 'trigger' ).delay( 200 )
+  // Test-out transaction synchronization on tag
+  rs
+    .union( [
+      rs.crankshaft( tag ).flow( 'counters' ),
+      rs.crankshaft( tag ).flow( 'trigger' ).delay( 200 )
     ], { tag: tag } )
     
     .trace( 'after union' )
     .greedy()
-  ;
-  
-  rs
-    .beat( 1000 )
-    .trigger()
   ;
 } );
