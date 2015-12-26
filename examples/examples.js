98418bfe0aef44b539b4469428bef8048495f49c
Jean Vincent <uiteoi@gmail.com>
2015-12-26 12:33:21 +0000

    examples.js: generalize use of Singleton(), set_output() and output() to eliminate var definitions

--- examples/examples.js
+++ examples/examples.js
@@ -5,26 +5,13 @@
 module.exports = function( servers ) {
   'use strict';
   
-  var rs  = require( 'toubkal' )
-    , RS  = rs.RS
+  var rs     = require( 'toubkal' )
+    , RS     = rs.RS
     , extend = RS.extend
-    , log = RS.log
-    , de  = true
-    , ug  = log.bind( null, 'examples' )
-  ;
-  
-  require( 'toubkal/lib/server/file.js'                 );
-  require( 'toubkal/lib/socket_io/socket_io_clients.js' );
-  
-  /* ------------------------------------------------------------------------------------
-     de&&ug()
-  */
-  var de = true, ug = rs.RS.log.bind( null, 'examples' );
-  
-  /* ------------------------------------------------------------------------------------
-     Build toubkal-min.js then listen on servers
-  */
-  var assets = require( 'toubkal/lib/server/client_assets.js' )
+    , log    = RS.log.bind( null, 'examples' )
+    , de     = true
+    , ug     = log
+    , assets = require( 'toubkal/lib/server/client_assets.js' )
     , toubkal_min = assets.toubkal_min()
     , react_js = assets.react.watch()
   ;
@@ -102,32 +89,41 @@ module.exports = function( servers ) {
     
     .trace( 'database tables' )
     .set_output( 'tables' )
+    
+    .Singleton( 'clients', function( source, options ) {
+      return source
+        .dispatch( servers.socket_io_clients(), function( source, options ) {
+          
+          return source.through( this.socket );
+        }, { single: true } )
+      ;
+    } )
+    
+    .clients()
   ;
-  
-  var database = rs.dispatch( rs.output( 'tables' ), function( source, options ) {
-    return source
-      .configuration( { 'filepath': this.path, 'flow': this.name, 'base_directory': __dirname  } )
-    ;
-  }, { single: true } );
   
   // Serve database to socket.io clients
   // ToDo: add option to dispatch() to use a union() as dispatcher instead of passthrough()
   // ToDo: or maybe, make path_through() a union() so that it becomes a controllet
   // ToDo: or just deprecate path_through() altogether
-  var clients_input = rs.union( [ database, rs.output( 'tables' ) ], { name: 'clients_input' } );
-  
-  var clients_output = clients_input
-    .dispatch( servers.socket_io_clients(), function( source, options ) {
-      
-      return source.through( this.socket );
-    }, { single: true } )
+  rs
+    .Singleton( 'database', function( source, tables, options ) {
+      return rs
+        .dispatch( tables, function( source, options ) {
+          return source
+            .configuration( { 'filepath': this.path, 'flow': this.name, 'base_directory': __dirname  } )
+          ;
+        }, { single: true } )
+      ;
+    } )
+    
+    .database( rs.output( 'tables' ) )
+    
+    .clients()
   ;
   
-  // Make encapsulated dataflow for data processors
-  var clients = rs.encapsulate( clients_input, clients_output );
-  
   // Require examples' data processors
-  var data_processors = rs
+  rs
     .directory_entries()
     
     .filter( [ { extension: 'js', depth: 2 } ] )
@@ -137,15 +133,21 @@ module.exports = function( servers ) {
     } )
     
     .trace( 'data processors' )
+    .set_output( 'data_processors' )
   ;
   
-  rs.dispatch( data_processors, function() {
-    // ToDo: cleanup required when removing a data processor, need to disconnect data from database and clients
+  rs.dispatch( rs.output( 'data_processors' ), function() {
+    // ToDo: cleanup required when removing a data processor, need to disconnect data from database and clients, and remove from require cache
+    // ToDo: allow to hot-reload data-processor
     
-    var data_processor = './' + this.path;
+    var data_processor = './' + this.path
+      , path = require.resolve( data_processor )
+    ;
     
     de&&ug( 'require data processor:', data_processor );
     
-    require( data_processor )( database, clients );
+    delete require.cache[ path ];
+    
+    require( path )( rs.database(), rs.clients() );
   } );
 } // module.exports
