9648a37687c36b43adff289c64a3e2ba0cde4c84
Jean Vincent <uiteoi@gmail.com>
2016-03-24 23:51:46 +0100

    manual test for has_none(): add comment to show expected output

--- test/manual/has_none.js
+++ test/manual/has_none.js
@@ -1,3 +1,30 @@
+/*
+  Should show:
+ 
+  2016/03/23 11:42:05.728 - Trace( has_none )..fetched(),  {
+    "no_more": true,
+    "adds": [],
+    "source": "Set.Output( has_none #11 )",
+    "query": [
+      {}
+    ],
+    "query_changes": [
+      [],
+      [
+        {}
+      ]
+    ]
+  }
+  ----------------------- + 1 second 30 milliseconds -----------------------
+  2016/03/23 11:42:06.758 - Trace( has_none )..add(),  {
+    "values": [
+      {
+        "id": 1
+      }
+    ]
+  }
+
+*/
 require( 'toubkal/lib/core' )
   .Singleton( 'source', function( source, options ) {
     return source.set( [ { id : 2 } ] )
