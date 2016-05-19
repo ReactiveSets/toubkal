858837730fe66a88b7fc0cc17d4ccc89f688e120
Jean Vincent <uiteoi@gmail.com>
2016-05-20 00:52:32 +0100

    fix filter_map() static attribute, add manual test

---
+++ test/manual/filter_map.js
@@ -0,0 +1,29 @@
+var rs = require( 'toubkal' );
+
+rs
+  .set( [
+    { flow: 'a', id: 1, b_id: 1 },
+    { flow: 'a', id: 2, b_id: 1 },
+    { flow: 'a', id: 3, b_id: 3 }
+  ] )
+  
+  .trace( 'source', { all: true } )
+  
+  .filter_map( rs
+    .set( [
+      { flow: 'b', id: 1 },
+      { flow: 'b', id: 2 },
+      { flow: 'b', id: 3 }
+    ] )
+    
+    .trace( 'parent', { all: true } )
+    
+    , 'a', { b_id: 'id' }
+  )
+  
+  .trace( 'out' )
+  
+  .filter( [ { b_id: 2 }, { b_id: 1 } ] )
+  
+  .greedy()
+;
