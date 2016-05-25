e7809d18a38e04b9673b269ffd0fcd21dd736e0d
Jean Vincent <uiteoi@gmail.com>
2016-05-25 17:42:19 -0400

    rename lib/core/cache.js lib/core/stateful.js; test/manual/cache.js: use core cache()

--- test/manual/cache.js
+++ test/manual/cache.js
@@ -1,20 +1,5 @@
-var rs = require( 'toubkal/lib/core' );
+require( 'toubkal/lib/core' )
 
-rs
-  .Compose( 'cache', function( source, options ) {
-    var rs = source.namespace()
-      , output = rs.pass_through( options )
-    ;
-    
-    return source
-      .filter( rs.query_updates( output ), options )
-      
-      .set()
-      
-      .union( [ output ] )
-    ;
-  } ) // cache()
-  
   .set( [
     { id: 1 },
     { id: 2 },
