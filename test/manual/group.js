374eada976348164653457631e3f0de54a2a97fd
Jean Vincent <uiteoi@gmail.com>
2017-03-30 23:24:34 +0100

    group() manual testing: display sets' keys; trace operations before and after a downstream set

--- test/manual/group.js
+++ test/manual/group.js
@@ -1,4 +1,7 @@
-var rs = require( 'toubkal/lib/core' );
+var rs  = require( 'toubkal/lib/core' )
+  , RS  = rs.RS
+  , log = RS.log
+;
 
 var cities = rs
   .set( [
@@ -10,14 +13,29 @@ var cities = rs
     { country: 'France' , city: 'Lille'       },
     { country: 'France' , city: 'Caen'        },
     { country: 'England', city: 'Southampton' }
-  ], { key: [ 'country', 'city' ] } )
+  ], { key: [ 'country', 'city' ], name: 'cities' } )
 ;
- 
-cities
-  .group( by_country, { key: [ 'country' ], initial_groups: [ { country: 'Germany' }, { country: 'England' } ] } )
+
+var cities_by_country = cities
+  .group( by_country, {
+    initial_groups: [ { country: 'Germany' }, { country: 'England' } ],
+    name: 'cities_by_country'
+  } )
+;
+
+var cities_by_country_set = cities_by_country
+  .trace( 'cities by country' )
   
-  .trace( 'cities by country' ).greedy()
+  .set( [], { name: 'cities_by_country_set' } )
 ;
+
+cities_by_country_set  
+  .trace( 'cities by country set' )
+  
+  .greedy()
+;
+
+log( 'cities_by_country and cities_by_country_set keys:', typeof cities_by_country._key, cities_by_country._key, cities_by_country_set._key );
 
 cities._add   ( [ { country: 'Germany', city: 'Berlin' } ] );
 cities._remove( [ { country: 'Germany', city: 'Berlin' } ] );
