b8ed3e6c19c300b8bfb6187d5b402f1416a4af41
Jean Vincent <uiteoi@gmail.com>
2017-04-03 11:14:55 +0100

    add manual test for dispatch

---
+++ test/manual/dispatch.js
@@ -0,0 +1,31 @@
+var rs     = require( 'toubkal' )
+  , RS     = rs.RS
+  , extend = RS.extend
+  , de     = true
+  , a      = rs.set( [ { id: 1, a: { a: true } } ] )
+  , b      = rs.pass_through()
+  , a_in   = a._input
+  , b_in   = b._input
+  
+  , c      = a.debug( de && 'a', { all: true, pick: { id: '.id', a: '.a.a' } } )
+  
+              .dispatch( b.debug( de && 'b' ), function( a ) {
+                return a //.debug( de && 'in' )
+              }, { branches_tag: 'branches tag', name: 'c' } )
+              
+              //.pass_through()
+              .debug( de && 'c', { tag: 'b' } )
+              .greedy()
+              
+  , forks  //= [ 'b' ]
+  , t      = { id: 'add to b', forks: forks }
+  , o      = { _t: t }
+  , t_more = extend( {}, t, { more: true } )
+  , o_more = { _t: t_more }
+;
+
+c.greedy();
+
+b_in.listen( 0, [ { id: 2 } ], o_more )
+b_in.listen( 0, [           ], o      )
+//a_in.listen( 0, [           ], o      )
