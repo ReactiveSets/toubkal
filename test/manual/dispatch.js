76d7e29c0cac18325d3807344e4efabc8ea0872d
Jean Vincent <uiteoi@gmail.com>
2017-04-15 18:58:57 +0100

    dispatch() manual test: remove + add branches in a transaction

--- test/manual/dispatch.js
+++ test/manual/dispatch.js
@@ -1,20 +1,21 @@
 var rs     = require( 'toubkal' )
   , RS     = rs.RS
+  , uuid   = RS.uuid.v4
   , extend = RS.extend
-  , de     = true
+  , de     = false
   , a      = rs.set( [ { id: 1, a: { a: true } } ] )
   , b      = rs.pass_through()
   , a_in   = a._input
   , b_in   = b._input
   
-  , c      = a.debug( de && 'a', { all: true, pick: { id: '.id', a: '.a.a' } } )
+  , c      = a.debug( 1 && 'a', { pick: { id: '.id', a: '.a.a' } } )
   
-              .dispatch( b.debug( de && 'b' ), function( a ) {
+              .dispatch( b.debug( 1 && 'b' ), function( a ) {
                 return a.debug( de && 'in' )
-              }, { branches_tag: 'branches tag', name: 'c', single: true } )
+              }, { name: 'c', single: de } )
               
               //.pass_through()
-              .debug( de && 'c-trace', { tag: 'b' } )
+              .debug( 1 && 'c-trace', { tag: 'b' } )
               .greedy()
               
   , forks  //= [ 'b' ]
@@ -30,9 +31,14 @@ c.greedy();
 b_in.listen( 0, [ { id: 2 } ], o_more )
 b_in.listen( 0, [           ], o      )
 
+t_more.id = t.id = uuid();
+
 // remove plus add in a transaction
 b_in.listen( 1, [ { id: 2 } ], o_more )
-b_in.listen( 0, [ { id: 2 } ], o_more )
-b_in.listen( 0, [           ], o      )
+b_in.listen( 0, [ { id: 2 }, { id: 3 } ], o )
+//b_in.listen( 0, [ { id: 2 } ], o_more )
+//b_in.listen( 0, [], o )
 
-//a_in.listen( 0, [           ], o      )
+//a_in.listen( 0, [], o )
+
+RS.log( RS.log.pretty( c._input.source.source.transactions ) )
