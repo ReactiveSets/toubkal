be92e7b87045d3edda8d964bfc699e9e166dad38
Jean Vincent <uiteoi@gmail.com>
2018-04-12 23:56:12 +0100

    cancelable: WIP, keep debugging, fix API, improve documentation

--- cancelable.js
+++ cancelable.js
@@ -1,13 +1,13 @@
-var cancelable = require( './lib/util/cancelable.js' )
-  , log        = require( './lib/util/console_logger.js' )().bind( null, 'cancelable_test' )
+var cancelable = require( './lib/util/cancelable' )
+  , log        = require( './lib/util/console_logger' )().bind( null, 'cancelable_test' )
   , limiter    = cancelable.limiter
   , map        = cancelable.map
   , values     = [ 1, 4, -1, 6, 7, -3, 24, 0, -8 ]
   , cancel     = map( values, 2, increment_delayed, done )
 ;
 
-// abort after 25 milliseconds, i.e. after only 4 source values have been processed
-setTimeout( function() { log( 'cancelling' ); cancel( 1 && 'timeout' ); log( 'canceled' ) }, 290 );
+// abort after 290 milliseconds, i.e. after only 4 source values have been processed
+setTimeout( function() { log( 'cancelling' ); cancel( 'timeout' ); log( 'canceled' ) }, 290 );
 
 function increment_delayed( value, next ) {
   // log( 'synchronous increment:', value ); return next( null, value + 1 );
@@ -20,15 +20,16 @@ function increment_delayed( value, next ) {
   function increment() {
     log( 'increment:', value );
     
-    next( null, value + 1 )
+    next( 0, value, 1 );
+    next( 0, value + 1 );
   }
   
   function cancel( reason ) {
     log( 'cancel():', reason );
     
     clearTimeout( timeout )
-
-    // next will never be called for this instance
+    
+    next( reason );
   }
 } // increment_delayed()
 
