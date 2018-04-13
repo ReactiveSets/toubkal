fcfbd21d02598213631094f42b96864b1b809251
Jean Vincent <uiteoi@gmail.com>
2018-04-13 12:17:44 +0100

    cancelable: WIP, improve documentation, manual testing for shared limiter

--- cancelable.js
+++ cancelable.js
@@ -3,36 +3,54 @@ var cancelable = require( './lib/util/cancelable' )
   , limiter    = cancelable.limiter
   , map        = cancelable.map
   , values     = [ 1, 4, -1, 6, 7, -3, 24, 0, -8 ]
-  , cancel     = map( values, 2, increment_delayed, done )
+  , limit      = limiter( 2 ) // shared limiter
+  , delay      = 100
 ;
 
-// abort after 290 milliseconds, i.e. after only 4 source values have been processed
-setTimeout( function() { log( 'cancelling' ); cancel( 'timeout' ); log( 'canceled' ) }, 290 );
+setTimeout( round( 0 ), delay * 10.5 );
 
-function increment_delayed( value, next ) {
-  // log( 'synchronous increment:', value ); return next( null, value + 1 );
+round( 1 );
+round( 2 );
+
+function round( round ) {
+  var debug  = log.bind( null, 'round ' + round + ',' )
+    , cancel = map( values, limit, increment_delayed, done )
+  ;
   
-  //
-  var timeout = setTimeout( increment, 100 );
+  return verbose_cancel;
   
-  return cancel;
-  
-  function increment() {
-    log( 'increment:', value );
+  function increment_delayed( value, next ) {
+    // return debug( 'synchronous increment:', value ), next( 0, value, 1 ), next( 0, value + 1 );
     
-    next( 0, value, 1 );
-    next( 0, value + 1 );
+    var timeout = setTimeout( increment, delay );
+    
+    return cancel;
+    
+    function increment() {
+      debug( 'increment:', value );
+      
+      //next( 0, value, 1 ); // flat map current source value
+      next( 0, value + 1 );
+    }
+    
+    function cancel( reason ) {
+      debug( 'cancel():', reason, ', value:', value );
+      
+      clearTimeout( timeout ); // effectively cancelling the call to increment()
+      
+      next( reason ); // effectively canceled
+    }
+  } // increment_delayed()
+  
+  function done( error, values ) {
+    debug( 'done():', error, values ) // [ 2, 5, 0, 7 ];
   }
   
-  function cancel( reason ) {
-    log( 'cancel():', reason );
+  function verbose_cancel() {
+    debug( 'cancelling' );
     
-    clearTimeout( timeout )
+    cancel( 'timeout' );
     
-    next( reason );
-  }
-} // increment_delayed()
-
-function done( error, values ) {
-  log( 'done():', error, values ) // [ 2, 5, 0, 7 ];
-}
+    debug( 'canceled' )
+  } // verbose_cancel()
+} // round()
