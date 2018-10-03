848409af03bf3fa2213625608322392fd4dcc490
Jean Vincent <uiteoi@gmail.com>
2018-10-03 12:30:38 +0100

    manual test fetch regex: play with reactive complex expressions

--- test/manual/fetch_regex.js
+++ test/manual/fetch_regex.js
@@ -1,22 +1,33 @@
 var rs = require( 'toubkal' )
-  , users = rs
+
+  , authorizations = rs.set( [ { id: [ '<=<', 1, 5 ] } ], { key: ['dummy'] } )
+  
+  , users_source = rs.pass_through()
+  
+  , users = users_source
       .set( [
-        { id: 1, name: 'Jean Vincent'   },
-        { id: 2, name: 'Khalifa Nassik' },
-        { id: 3, name: 'Samy Vincent'   },
+        { id: 1, name: 'John Snape'   },
+        { id: 2, name: 'Natalie Lee' },
+        { id: 3, name: 'Jeremy De Souza'   },
       ] )
       
       .pass_through()
       
       .trace( 'from set', { all: true } )
       
-      .filter( [ { id: [ 'in', [ '$', [ 1, 2, 3 ] ] ] } ] )
+      .filter( authorizations )
       
       .union( [] )
       
       .pick( [ 'id', 'name', 'age' ] )
-      
+  
   , source = rs.pass_through()
+;
+
+users
+  .optimize()
+  .trace( 'users' )
+  .greedy()
 ;
 
 source
@@ -28,5 +39,10 @@ source
 //source._add( [ { id: 34 } ] );
 //source._add( [ { id: 35, query: [ { name: 'Jean Vincent' } ] } ] );
 //source._add( [ { id: 36, query: [ { name: [ 'match', 'Jean Vincent' ] } ] } ] );
-source._add( [ { id: 36, query: [ { id: [ '<=', 5 ], name: [ 'match', 'k', '||', [ '_', 'id' ], '<=', 3, '&&', 'match', [ 'RegExp', 's', 'i' ] ] } ] } ] );
+//source._add( [ { id: 36, query: [ { id: [ '<=', 5 ], name: [ 'match', 'k', '||', [ '_', 'id' ], '<=', 3, '&&', 'match', [ 'RegExp', 's', 'i' ] ] } ] } ] );
 //source._add( [ { id: 36, query: [ { id: [ '>', 1 ] } ] } ] );
+
+users_source._add( [ { id: 4, name: 'Jack London' } ] )
+
+// authorizations._remove( [ { id: [ '<=<', 1, 5 ] } ] );
+authorizations._update( [ [ { id: [ '<=<', 1, 5 ] }, { id: [ 'in_set', '<<=', 1, 2, '<<=', 3, 5 ] } ] ] )
