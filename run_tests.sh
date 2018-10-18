7a5024fed05f5420716cc6f83472711f6c590be1
Jean Vincent <uiteoi@gmail.com>
2018-10-18 11:26:52 +0100

    run_tests.sh: fix forever missing in ui-only tests; display message to look for test/server/http.js output into http.out

--- run_tests.sh
+++ run_tests.sh
@@ -1,33 +1,44 @@
 #!/bin/sh
-# Runs Connected Sets tests
 #
-# Assumes that node_modules/excess links to this repository root. This can be achieved
-# by running 'npm link' after which 'ls -l node_modules' should show:
-#   excess -> ../.
+# Runs toubkal tests
+#
+# Assumes that ./node_modules/toubkal links to this root repository
+# to allow to test scripts to require( "toubkal" ).
+#
+# This can be achieved by running:
+#   npm link
+#
+# After which
+#   ls -l node_modules
+#
+# should show:
+#   lrwxrwxrwx toubkal -> ..
 #
 # Usage:
 #   ./run_tests.sh [target]
 #
 # Where target can be:
 #   empty-string: uses npm test to run all tests, possibly running pretest and posttest scripts
-#   "all"       : all coffee source are compiled and executed
-#   other       : ./test/src/$1.coffee is compiled and ./test/lib/$1.js is executed
+#   all     : all coffee source are compiled and executed using npm test
+#   ui      : all ui tests launching test/server/http.js, redirecting results to http.out
+#   <other> : ./test/src/$1.coffee is compiled and ./test/lib/$1.js is executed
 #
-# Tests results are then available in test.out
+# Tests results are then available in test.out and http.out for ui tests
 #
 if [ "$1" = "" ]; then
   npm test 2>&1 > test.out || echo " test failed"
 else
   if [ "$1" = "ui" ]; then
-    sh ./test/bin/pre_test.sh
+    printf "test/server/http.js output:\n\n" > http.out
+    ./node_modules/forever/bin/forever -m 1 -a -o http.out -e http.out start test/server/http.js
     
-    sleep 3
+    sleep 18
   fi
   
   ./test/bin/tests.sh $1 2>&1 > test.out || echo " test failed"
   
   if [ "$1" = "ui" ]; then
-    sh ./test/bin/post_test.sh
+    ./node_modules/forever/bin/forever stop test/server/http.js
   fi
 fi
 
@@ -37,3 +48,7 @@ grep '[[:cntrl:]]' test.out
 
 echo
 echo "To get full test results: less -R test.out"
+
+if [ "$1" = "ui" ] || [ "$1" = "" ]; then
+  printf "\nTo see test/server/http.js stdout and stderr: less http.out\n"
+fi
--- test/bin/pre_test.sh
+++ test/bin/pre_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 which forever > /dev/null 2>&1 && \
-echo "test/server/http.js output:\n" > http.out && \
+printf "test/server/http.js output:\n\n" > http.out && \
 forever -m 1 -a -o http.out -e http.out start test/server/http.js && \
 sleep 3 || \
 echo 'forever not found, not starting test/http.js' 1>&2
