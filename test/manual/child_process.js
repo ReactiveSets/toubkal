9b8aee1f1720ec76797aaa8123db91e21fd9999d
Jean Vincent <uiteoi@gmail.com>
2018-11-12 02:32:57 +0000

    child_process_exec() tests: test add, remove and update in a transaction

--- test/manual/child_process.js
+++ test/manual/child_process.js
@@ -1,8 +1,27 @@
 require( "toubkal" )
   
-  .once( 1 ) // child_process_exec does not yet work on initial fetch
+  .once( 1 ) // emit_operations() does respond to initial fetch because it is transactional
   
-  .alter( { command: "echo hello world on stdout; echo to stderr 1>&2" } )
+  .map( function( _ ) {
+    return {
+      adds: [ { command: "echo hello world on stdout" } ],
+      removes: [ { command: "echo to stderr 1>&2" } ],
+      updates: [ [ { command: "echo removed" }, { command: "echo added" } ] ]
+    }
+  } )
+  
+  .emit_operations()
+  
+  /*
+  .once()
+  
+  .flat_map( function( _ ) {
+    return [
+      { command: "echo hello world on stdout" },
+      { command: "echo to stderr 1>&2" }
+    ]
+  } )
+  */
   
   .child_process_exec()
   
