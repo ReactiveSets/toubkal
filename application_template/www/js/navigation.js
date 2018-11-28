9fef9195a2bfa65764e259d7b41a56d3c3a045b5
Jean Vincent <uiteoi@gmail.com>
2018-11-28 19:00:29 +0000

    application template navigation.js: remove unsused variables, add copyright statement

--- application_template/www/js/navigation.js
+++ application_template/www/js/navigation.js
@@ -1,21 +1,22 @@
-/*  
-  navigation.js
-  -------------
-  
-  Licence
+/*
+    Copyright (c) 2013-2018, Reactive Sets
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 ( this.undefine || require( 'undefine' )( module, require ) )()
 ( 'navigation', [ [ 'rs', 'toubkal' ] ], function( rs ) {
   "use strict";
-  
-  var RS           = rs.RS
-    , extend       = RS.extend
-    , uuid_v4      = RS.uuid.v4
-    , add_class    = RS.add_class
-    , remove_class = RS.remove_class
-  ;
   
   return rs
     
