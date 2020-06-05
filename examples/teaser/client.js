1472263a70f661848e197dab08abe14ef812b962
Jean Vincent <uiteoi@gmail.com>
2020-06-05 14:10:38 +0200

    examples/teaser/client.js: use option untag instead of deprecated option tag

--- examples/teaser/client.js
+++ examples/teaser/client.js
@@ -2,7 +2,7 @@
 
     The MIT License (MIT)
 
-    Copyright (c) 2013-2017, Reactive Sets
+    Copyright (c) 2013-2020, Reactive Sets
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
@@ -40,7 +40,7 @@
     .cache( { synchronizing: rs.socket_io_synchronizing() } )
     
     // Pull teaser/sales dataflow
-    .flow( 'teaser/sales_year', { tag: 'synchronizing' } )
+    .flow( 'teaser/sales_year', { untag: 'synchronizing' } )
     
     // Limit pulled sales to some years
     .filter( [
