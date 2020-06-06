1670533f39b0c1718471aa5e8d8aee95cd0fc668
Jean Vincent <uiteoi@gmail.com>
2020-06-06 23:29:37 +0000

    test/server/test-strategy.js: small refactoring

--- test/server/test-strategy.js
+++ test/server/test-strategy.js
@@ -4,26 +4,23 @@
     Copyright (C) 2013, 2020, Reactive Sets
 
     Inspired from https://github.com/jboxman/koa-passport-oauth2-testing, licence: ISC
-    
-
 */
 
 'use strict';
 
 const passport = require( 'passport-strategy' )
-  , util       = require('util')
-  , profile    = require( './test-profile'    )
+  , util       = require( 'util' )
+  , profile    = require( './test-profile' )
 ;
 
 function Strategy( name, callback ) {
-  if( !name || name.length === 0 ) {
-    throw new TypeError( 'DevStrategy requires a Strategy name' );
+  if ( !name || name.length === 0 ) {
+    throw new TypeError( 'Test strategy requires a strategy name' );
   }
 
   passport.Strategy.call( this );
   
   this.name  = name;
-  this._profile = profile;
   
   // Callback supplied to OAuth2 strategies handling verification
   this._callback = callback;
@@ -31,8 +28,8 @@ function Strategy( name, callback ) {
 
 util.inherits( Strategy, passport.Strategy );
 
-Strategy.prototype.authenticate = function() {
-  this._callback( null, null, this._profile, ( error, profile ) => {
+Strategy.prototype.authenticate = function( request, options ) {
+  this._callback( null, null, profile, ( error, profile ) => {
     this.success( profile );
   } );
 }
